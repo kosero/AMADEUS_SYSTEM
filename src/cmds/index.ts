3cc144d7422773f785e39e66e9f6ddce21f9cdac
kosero <kosero@tuta.io>
2025-12-05 20:17:18 +0300

    i forgot...

--- src/cmds/index.ts
+++ src/cmds/index.ts
@@ -1,7 +1,11 @@
 import * as nya from "./nya.js";
+import * as tts from "./tts.js";
+import * as register from "./register-cmd.js";
 
 const cmds = {
     [nya.data.name]: nya,
+    [tts.data.name]: tts,
+    [register.data.name]: register,
 };
 
 export default cmds;
